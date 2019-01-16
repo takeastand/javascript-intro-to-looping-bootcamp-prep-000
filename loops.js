713d47d851426c919452a42acf8124167be2f53e
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-16 04:01:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,3 +3,16 @@ for (let i = 1; i < 25; i++) {
   console.log("I am ${i} strange loop")
 }
 }
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+    return array;
+  } while (array.length > 0 && incrementVariable())
+}
